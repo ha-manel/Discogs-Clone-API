20d18790078ab778a69086e96fad3aa59a93c222
Manel <manelhammouche3@gmail.com>
2022-12-10 13:59:07 +0100

    Seed some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Record.create(title: 'Rumours', artist: 'Fleetwood Mac', year: 1977, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Rock')
+
+Record.create(title: 'Abbey Road', artist: 'Beatles', year: 1970, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Rock')
+
+Record.create(title: 'Nevermind', artist: 'Nirvana', year: 1991, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Rock')
+
+Record.create(title: 'Thriller', artist: 'Michael Jackson', year: 1982, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Pop')
+
+Record.create(title: 'Fine Line', artist: 'Harry Styles', year: 2019, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Pop')
+
+Record.create(title: '400 Degreez', artist: 'Juvenile', year: 1998, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Rap')
