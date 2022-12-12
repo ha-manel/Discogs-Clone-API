981eb9983e43bd351da7231f819da24308e1971c
Manel <manelhammouche3@gmail.com>
2022-12-12 19:15:57 +0100

    Fix bug in route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get "/api/v1/records", to: "records#index"
   post "/api/v1/record", to: "records#create"
-  delete "/api/v1/record/id", to: "records#destroy"
+  delete "/api/v1/record/:id", to: "records#destroy"
 end
