e5e2630f37062111745cea96adbcbdb866f40070
Manel <manelhammouche3@gmail.com>
2022-12-15 15:43:19 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 require 'faker'
 
-(1..20).each do |id|
+(1..20).each do
   Record.create!(
-    id: id,
     title: Faker::Music.album,
     artist: Faker::Music.band,
     year: Faker::Number.between(from: 1970, to: 2022),
