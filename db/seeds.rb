2ff11274eaad56444f72ebc132a4db94ec76bc58
Manel <manelhammouche3@gmail.com>
2022-12-15 13:52:40 +0100

    Seed some records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
-Record.create(title: 'Rumours', artist: 'Fleetwood Mac', year: 1977, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Rock')
+require 'faker'
 
-Record.create(title: 'Abbey Road', artist: 'Beatles', year: 1970, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Rock')
-
-Record.create(title: 'Nevermind', artist: 'Nirvana', year: 1991, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Rock')
-
-Record.create(title: 'Thriller', artist: 'Michael Jackson', year: 1982, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Pop')
-
-Record.create(title: 'Fine Line', artist: 'Harry Styles', year: 2019, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Pop')
-
-Record.create(title: '400 Degreez', artist: 'Juvenile', year: 1998, image_data: 'https://pyxis.nymag.com/v1/imgs/d9a/fa0/e564d1ffe4671db5c981f7f187d30c8a0e-pioneer-photo-album.2x.rsquare.w600.jpg', genre: 'Rap')
+(1..20).each do |id|
+  Record.create!(
+    id: id,
+    title: Faker::Music.album,
+    artist: Faker::Music.band,
+    year: Faker::Number.between(from: 1970, to: 2022),
+    genre: Faker::Music.genre
+  )
+end
