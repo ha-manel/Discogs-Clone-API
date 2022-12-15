42f067e1b560f08340376e7def9292d248b716d5
Manel <manelhammouche3@gmail.com>
2022-12-15 13:52:59 +0100

    Add default cover picture

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -13,7 +13,16 @@ class RecordsController < ApplicationController
   end
 
   def create
-    @record = Record.new(record_params)
+    if params[:image_data] == "null"
+      @record = Record.new()
+      @record.title = params[:title]
+      @record.artist = params[:artist]
+      @record.year = params[:year]
+      @record.genre = params[:genre]
+      @record.image_data.attach(io: File.open(Rails.root.join("app", "assets", "images", "default.png")), filename: 'default.png' , content_type: "image/png")
+    else
+      @record = Record.new(record_params)
+    end
     if @record.save
       render json: { status: :created, record: @record.as_json.merge({ image_data: url_for(@record.image_data) }) }
     else
--- app/models/record.rb
+++ app/models/record.rb
@@ -5,4 +5,12 @@ class Record < ApplicationRecord
   validates :genre, presence: true
 
   has_one_attached :image_data
+
+  after_commit :add_default_cover, on: [:create, :update]
+
+  private def add_default_cover
+    unless image_data.attached?
+      self.image_data.attach(io: File.open(Rails.root.join("app", "assets", "images", "default.png")), filename: 'default.png' , content_type: "image/png")
+    end
+  end
 end
